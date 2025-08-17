34484d9fa5086240da75886043d0f47dd48950ca
pknath769 <pknath769@gmail.com>
2025-08-18 03:03:56 +0530

    08:trpc-setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 // import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,12 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <TRPCReactProvider>
+       <html lang="en">
       <body
         className={`${inter.className}  antialiased`}
       >
         {children}
       </body>
     </html>
+    </TRPCReactProvider>
+
   );
 }
